03b2e550747210684d203685c4730991b93f501a
Kasey Arnold <kaseyarnold15@gmail.com>
2020-03-09 20:12:54 +0000

    changed responses in cli

--- lib/DND/CLI.rb
+++ lib/DND/CLI.rb
@@ -1,15 +1,15 @@
-class DND::CLI 
-  
- class UrbanDictionary::CLI
+class DND::CLI
+
+ class DND::CLI
     def start
-        puts "Welcome to the Monster Manual!"
+        puts "Welcome to the your local 5E Monster Menagerie!"
         puts "Please type in the monster you would like for me to look up."
         input = gets.chomp.capitalize
         DND::API.fetch(input)
         display_def
         more_options
         goodbye
-    end 
+    end
 
     def display_def
         DND::Monster.all.each.with_index(1) do |d,index|
@@ -17,26 +17,28 @@ class DND::CLI
             puts "#{index}. #{d.name} is a #{d.challenge_level} monster."
             puts "-----------------------------------------"
 
-        end 
-    end 
+        end
+    end
 
     def more_options
         puts "If you would like to know more about this monster, please enter the number associated with it."
         input = gets.chomp.to_i
         index = input - 1
         user_choice = DND::Monster.all[index]
-        puts " example : #{user_choice.example}"
-    end 
+        puts "The #{d.name} is a #{d.size} monster.
+        It has an armor class of #{d.armor_class}.
+        It is a #{d.type} of monster with an alignment of #{d.alignment}"
+    end
 
     def loop_or_exit
         puts "Would you like to search for more monsters? enter y/n"
         input = gets.chomp.downcase
         if input == "y"
              start
-        else 
+        else
           puts "{goodbye}"
-        end 
-    end  
+        end
+    end
 
     def goodbye
     DND::Monster.destroy_all
