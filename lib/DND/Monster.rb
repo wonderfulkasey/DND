00f664a1a6ba088710cbcd32b2439b3e957f6018
Kasey Arnold <kaseyarnold15@gmail.com>
2020-03-09 19:25:45 +0000

    added accessors to monster

--- lib/DND/Monster.rb
+++ lib/DND/Monster.rb
@@ -1,51 +1,20 @@
 class DND::Monster
 
-  attr_accessor :name, :size, :alignment, :challenge_rating
-  
-   @@all = []
+  attr_accessor :name, :type, :size, :alignment, :armor_class, :challenge_rating 
+    
+    @@all = []
 
-  def self.new_from_index_page(r)
-    self.new(
-      r.css("h2").text,
-      "https://api.open5e.com/monsters/?limit=5#{r.css("a").attribute("href").text}",
-      r.css("h3").text,
-      r.css(".position").text
-      )
-  end
+    def initialize(definition, example,term)
+        @name = name
+        @type = type
+        @size = size
+        @alignment = alignment
+        @armor_class = armor_class
+        @challenge_rating = challenge_rating
+        @@all << self 
+    end 
 
-  def initialize(name=nil, url=nil, location=nil, position=nil)
-    @name = name
-    @url = url
-    @location = location
-    @position = position
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.find(id)
-    self.all[id-1]
-  end
-
-  def name
-    @name ||= doc.css("div.c-4.nr.nt ul:nth-child(8) li").text
-     end
-
-  def size
-    @size ||= doc.css("div.c-4.nr.nt ul:nth-child(6) li").text
-   end
-
-  def alignment
-    @alignment ||= doc.css("div.c-4.nr.nt ul:nth-child(10) li:nth-child(1)").text.split("+").join(". Tel: +")
-     end
-
-  def challenge_rating
-    @challenge_rating ||= doc.css("div.c-4.nr.nt ul:nth-child(3) li").text.split(" (pictured)").join("")
-  end
-
-  def doc
-    @doc ||= Nokogiri::HTML(open(self.url))
-  end
-end
+    def self.all
+        @@all  
+    end 
+end 
