8f8a2a80ec8e641fc6245efcb9b395b838b8304b
Kasey Arnold <kaseyarnold15@gmail.com>
2020-03-10 19:04:22 +0000

    added more flavor text

--- lib/DND/CLI.rb
+++ lib/DND/CLI.rb
@@ -1,8 +1,10 @@
 class DND::CLI
 
     def start
+      puts "-----------------------------------------"
         puts "Welcome to the your local 5E Monster Menagerie!"
         puts "Please type in the monster you would like for me to look up."
+        puts "-----------------------------------------"
         input = gets.chomp.downcase
         DND::API.fetch(input)
         wowing
@@ -17,24 +19,24 @@ class DND::CLI
         @m=DND::Monster.all.last
         #  binding.pry
             sleep(1)
-            puts "Wow! The #{@m.name} is a terrifying monster!"
             puts "-----------------------------------------"
+            puts "Wow! The #{@m.name} is a terrifying monster!"
   end
 
 
     def more_options
         puts "Would you like to know more about this monster? enter y/n"
+        puts "-----------------------------------------"
         input = gets.chomp.downcase
 
         if input == "y"
         #input = gets.chomp.to_i
         #index = input - 1
         #user_choice = DND::Monster.all[index]
-            puts "The #{@m.name} is a #{@m.size} monster.
-            It has an armor class of #{@m.armor_class}.
-            It is a #{@m.type} of monster with an alignment of
-            #{@m.alignment}."
-
+            puts "-----------------------------------------"
+            puts "The #{@m.name} is a #{@m.size} sized monster with
+            an armor class of #{@m.armor_class}. It is of the #{@m.type}
+            variety, and can be #{@m.alignment} on the moral scale."
         else
           loop_or_exit
         end
@@ -43,12 +45,15 @@ class DND::CLI
 
     def loop_or_exit
         puts "Would you like to search for more monsters? enter y/n"
+        puts "-----------------------------------------"
         input = gets.chomp.downcase
         if input == "y"
              start
 
         else
+          puts "-----------------------------------------"
           puts "Farewell, and good luck on your quest!"
+          puts "-----------------------------------------"
         end
 
       end
