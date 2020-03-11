76ce2c257b9aeb4828faaedc5149c94a24c1ede1
Kasey Arnold <kaseyarnold15@gmail.com>
2020-03-11 01:08:20 +0000

    api metaprogramming

--- lib/DND/API.rb
+++ lib/DND/API.rb
@@ -5,31 +5,34 @@ class DND::API
 
          response = RestClient.get(url)
          response = JSON.parse(response)
+
+         monster.each {|monster_hash| DND::Monster.new(monster_hash)}
          #binding.pry
          #binding.pry
          #if !response.empty?
 
-          name = response["name"]
-          type = response["type"]
+        #  name = response["name"]
+        #  type = response["type"]
 
-          DND::Monster.new(name, type)
-          end
+    DND::Monster.new(name, type)
+  end
+end
 
 
-  def self.monster_info(info)
-        url = "www.dnd5eapi.co/api/monsters/#{name}"
+#  def self.monster_info(info)
+        #url = "www.dnd5eapi.co/api/monsters/#{name}"
 
-        response = RestClient.get(url)
-        response = JSON.parse(response)
+        #response = RestClient.get(url)
+      #  response = JSON.parse(response)
 
-        size = response["size"]
-        alignment = response["alignment"]
-        armor_class = response["armor_class"]
-        wisdom = response["wisdom"]
-        constitution = response["constitution"]
-        charisma = response["charisma"]
+        #size = response["size"]
+        #alignment = response["alignment"]
+        #armor_class = response["armor_class"]
+        #wisdom = response["wisdom"]
+        #constitution = response["constitution"]
+        #charisma = response["charisma"]
 
-        DND::Info.new(size, alignment, armor_class, wisdom, constitution, charisma)
-        end
+        #DND::Info.new(size, alignment, armor_class, wisdom, constitution, charisma)
+        #end
 
-    end
+    #end
--- lib/DND/Monster.rb
+++ lib/DND/Monster.rb
@@ -11,19 +11,29 @@ class DND::Monster
 
     def self.display_details(index)
         m = self.all[index]
-        @name = name
-        @type = type
-        @size = size
-        @alignment = alignment
-        @armor_class = armor_class
-        @wisdom = wisdom
-        @constitution = constitution
-        @charisma = charisma
-        @@all << self
+        puts "----------------"
+        #@name = name
+        puts "#{m.name}"
+        #@type =
+        puts "#{m.type}"
+        #@size = size
+        puts "#{m.size}"
+        #@alignment = alignment
+        puts "#{m.alignment}"
+        #@armor_class = armor_class
+        puts "#{m.armor_class}"
+        #@wisdom = wisdom
+        #@constitution = constitution
+        #@charisma = charisma
+        #@@all << self
     end
 
     def self.all
       @@all
+    end
+
+    def self.distroy_all
+      @@all.clear
     end
 
   end
