118ae6a72fe20364e4735d6e37d648b46d58f41f
Kasey Arnold <kaseyarnold15@gmail.com>
2020-03-22 00:03:14 +0000

    fixed url

--- lib/DND/API.rb
+++ lib/DND/API.rb
@@ -1,8 +1,8 @@
 class DND::API
 
   def self.fetch(name)
-        url = "www.dnd5eapi.co/api/monsters/#{name}"
-        response = RestClient.get(url)
+        website = "www.dnd5eapi.co/api/monsters/#{name}"
+        response = RestClient.get(website)
         response = JSON.parse(response)
 
           hash  = {
@@ -10,8 +10,11 @@ class DND::API
           "size" => response["size"],
           "type" => response["type"],
           "alignment" => response["alignment"],
-          "armor_class" => response["armor_class"]
-          "url" => response["url"]}
+          "armor_class" => response["armor_class"],
+          "url" => response["url"]
+        }
+
+
           DND::Monster.new(hash)
 
   #response.each {|fetch| DND::Monster.new(fetch)}
